f680e4c4214da48e397ffa07b9f0044a9f3f98e1
Krystal <lopezkrystal10@gmail.com>
2023-07-31 14:18:35 -0700

    added event handlers to book create component and imported css styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  const [books, setBooks] = useState([]); // set to an empty array, as books are going to be listed as an array of objects 
-  
+  const [books, setBooks] = useState([]); // set to an empty array, as books are going to be listed as an array of objects
+
+  //event handler creating a book
+  const createBook = (title) => {
+    console.log("need to add book with", title);
+  };
+
   return (
     <div className="App">
-     App
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
 
-function BookCreate() {
-  return <>
-    <div>
-      <p>This is  where we create the book </p>
-    </div>
-  </>
+  //updates title state to be hold the value of the user input
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  //submit event handler
+  const handleSubmit = (event) => {
+    event.preventDefault(); //prevents data from reloading
+    onCreate(title);
+    setTitle(""); // update our state, set to empty string this will rerender book create component and force the input element to show a value of an empty string,
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Title</label>
+          <input value={title} onChange={handleChange} />
+          <button>Create!</button>
+        </form>
+      </div>
+    </>
+  );
 }
-
 
 export default BookCreate;
