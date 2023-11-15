1254fdd6d2f3db0494e9a23774d188ab43fb412e
Krystal <lopezkrystal10@gmail.com>
2023-11-15 11:41:43 -0800

    updated delete and update functions

--- src/App.js
+++ src/App.js
@@ -17,11 +17,15 @@ function App() {
     fetchBooks();
   }, []);
 
-  // edits book by id 
-  const editBookById = (id, newTitle) => {
+  // edits book by id
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
@@ -29,7 +33,8 @@ function App() {
   };
 
   // deletes book by id
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
